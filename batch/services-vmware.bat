7092d67b24501384d753689621f8a6585c10a0bc
soiff <soiff@users.noreply.github.com>
2016-04-12 17:21:20 +0800

    Update services-vmware.bat

--- batch/services-vmware.bat
+++ batch/services-vmware.bat
@@ -1,4 +1,29 @@
 @echo off
 
+setlocal enabledelayedexpansion
+
 set services="VMAuthdService" "VMnetDHCP" "VMware NAT Service" "VMUSBArbService" "VMwareHostd" "LanmanWorkstation"
-for %%s in (%services%) do net %1 %%s
+
+for %%c in (%*) do (
+	if "x%%c" equ "xstart" (
+		for %%s in (%services%) do (
+			set /p "ign=try to %%c service %%s ... " <nul
+			set done=fail
+			net /y start %%s>nul 2>&1 && set done=done
+			echo !done!
+		)
+	) else if "x%%c" equ "xstop" (
+		for %%s in (%services%) do (
+			set /p "ign=try to %%c service %%s ... " <nul
+			set done=fail
+			net /y stop %%s>nul 2>&1 && set done=done
+			echo !done!
+		)
+	) else (
+		echo invalid command for net : %%c
+	)
+)
+
+endlocal
+
+set /p "ign=all done, press enter key to exit ... "
