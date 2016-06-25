b991ec969e08dd176fa607f7c053b4fc984500a3
unknown <z0228@dtdream.com>
2016-06-25 16:22:14 +0800

    add new route after delete defalt route

--- batch/vpn.bat
+++ batch/vpn.bat
@@ -19,6 +19,8 @@ for /f "usebackq tokens=1,2* delims=:" %%a in (`ipconfig`) do (
         echo %%a|findstr /c:"Media State">nul && set state=%%b
         if "x!state!" neq "x Media disconnected" (
             set /a index+=1
+            if !index! equ 3 set network=%%~b
+            if !index! equ 4 set mask=%%~b
             if !index! equ 5 set gateway=%%~b
         ) else (
             set begin=
@@ -30,7 +32,9 @@ for /f "usebackq tokens=1,2* delims=:" %%a in (`ipconfig`) do (
 :: drop default gateway
 if "x!gateway: =!" neq "x" (
     set /p ign=going to delete default route with gateway !gateway! ... <nul
-    route delete 0.0.0.0 mask 0.0.0.0 %gateway%>nul 2>&1 && echo done.
+    route delete 0.0.0.0 mask 0.0.0.0 !gateway!>nul 2>&1 && echo done.
+    set /p ign=going to add new route: !network! !mask! !gateway! ... <nul
+    route add !network! mask !mask! !gateway!>nul 2>&1 && echo done.
     :: flush dns
     set /p ign=going to refresh dns settings ... <nul
     ipconfig /flushdns>nul && echo done.
