e5e67bbee60bbf59321bf56eeed6e809baf8d8d6
soiff <wittcnezh@foxmail.com>
2016-06-28 10:45:11 +0800

    add maven script

--- batch/maven.bat
+++ batch/maven.bat
@@ -78,7 +78,12 @@ echo   %~n0 deploy -r:http://repo1.maven.org/maven2/ -m:groupId:artifactId:versi
 goto :exit
 
 :install
-call mvn install:install-file -DgroupId=!groupId! -DartifactId=!artifactId! -Dversion=!version! -Dpackaging=!packaging! -Dfile=!file!
+set done=
+call mvn install:install-file -DgroupId=!groupId! -DartifactId=!artifactId! -Dversion=!version! -Dpackaging=!packaging! -Dfile=!file!>nul 2>nul && set done=1
+if not defined done (
+	set /p ign=installation failed, try to copy it to local repository ... <nul
+	call :cp2m2 !groupId! !artifactId! !version! !file! && echo done.
+)
 goto :exit
 
 :deploy
@@ -88,7 +93,16 @@ goto :exit
 :get
 set success=
 call mvn dependency:get -DrepoUrl=!repoUrl! -Dartifact=!groupId!:!artifactId!:!version!:!packaging!
-if defined targetDir (
+call :cp !groupId! !artifactId! !version! !targetDir!
+goto :exit
+
+:cp2m2
+call :debug %~0 %*
+set _cp_groupId=%1
+set _cp_artifactId=%2
+set _cp_version=%3
+set _cp_file=%4
+if defined _cp_file (
 	set repository=%USERPROFILE%\.m2\repository
 	set localTmp=%TEMP%\%~n0_tmp
 	type %USERPROFILE%\.m2\settings.xml|findstr /c:localRepository>!localTmp!
@@ -98,18 +112,51 @@ if defined targetDir (
 			set localRepository=%%r
 		)
 	)
-	set filename=!artifactId!-!version!.jar
-	set relative=!groupId:.=\!\!artifactId!\!version!\!filename!
+	set filename=!_cp_artifactId!-!_cp_version!.jar
+	set folder=!_cp_groupId:.=\!\!_cp_artifactId!\!_cp_version!
+	set relative=!folder!\!filename!
 	call :debug local repository: !localRepository!
 	call :debug repository: !repository!
 	call :debug file relative to repository: !relative!
 	if exist "!localRepository!\!relative!" (
-		copy /b "!localRepository!\!relative!" !targetDir!\!filename!
+		echo try to copy !_cp_file! "!localRepository!\!relative!"
+		mkdir !localRepository!\!folder!
+		copy /b !_cp_file! "!localRepository!\!relative!"
 	) else if exist "!repository!\!relative!" (
-		copy /b "!repository!\!relative!" !targetDir!\!filename!
+		echo try to copy !_cp_file! "!repository!\!relative!"
+		mkdir !repository!\!folder!
+		copy /b !_cp_file! "!repository!\!relative!"
 	)
 )
-goto :exit
+goto :eof
+
+:cp
+set _cp_groupId=%1
+set _cp_artifactId=%2
+set _cp_version=%3
+set _cp_dir=%4
+if defined _cp_dir (
+	set repository=%USERPROFILE%\.m2\repository
+	set localTmp=%TEMP%\%~n0_tmp
+	type %USERPROFILE%\.m2\settings.xml|findstr /c:localRepository>!localTmp!
+	set localRepository=
+	for /f "usebackq tokens=2 delims=<> " %%r in (!localTmp!) do (
+		if not defined localRepository (
+			set localRepository=%%r
+		)
+	)
+	set filename=!_cp_artifactId!-!_cp_version!.jar
+	set relative=!_cp_groupId:.=\!\!_cp_artifactId!\!_cp_version!\!filename!
+	call :debug local repository: !localRepository!
+	call :debug repository: !repository!
+	call :debug file relative to repository: !relative!
+	if exist "!localRepository!\!relative!" (
+		copy /b "!localRepository!\!relative!" !_cp_dir!\!filename!
+	) else if exist "!repository!\!relative!" (
+		copy /b "!repository!\!relative!" !_cp_dir!\!filename!
+	)
+)
+goto :eof
 
 :mvnnotfound
 echo mvn not in PATH, program is going to exit.
