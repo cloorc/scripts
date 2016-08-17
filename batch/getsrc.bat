6f6a9e972c52773c6ea65264b9ba602faf26d90e
soiff <wittcnezh@foxmail.com>
2016-08-17 15:34:26 +0800

    add getsrc script to get maven sources

---
+++ batch/getsrc.bat
@@ -0,0 +1,52 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+axel --version>/nul 2>&1 && set is_axel_exist=true
+
+if not defined is_axel_exist goto :command_not_found
+
+set repo=http://repo2.maven.org/maven2
+
+set groupId=
+set artifactId=
+set version=
+
+for %%e in ( %* ) do (
+	if %%e gtr 0 (
+		if not defined groupId (
+			set groupId=%%e
+		) else (
+			if not defined artifactId (
+				set artifactId=%%e
+			) else (
+				if not defined version (
+					set version=%%e
+				)
+			)
+		)
+	)
+)
+
+if defined groupId if defined artifactId if defined version goto :execute
+goto :invalid_parameters
+
+:execute
+set out=!artifactId!-!version!-sources.jar
+set target=!repo!/!groupId:.=/!/!artifactId!/!version!/!out!
+echo going to get: !target!
+axel -n 4 -o !out! !target!
+goto :end
+
+:invalid_parameters
+echo invalid arguments found.
+echo coordinate: !groupId!:!artifactId!:!version!
+goto :end
+
+:command_not_found
+echo command axel is not found.
+goto :end
+
+:end
+endlocal
+goto :eof
