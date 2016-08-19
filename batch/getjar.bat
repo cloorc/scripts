698c6b5e49a69db787a8eaa78ae42f7edbad80a5
soiff <wittcnezh@foxmail.com>
2016-08-19 11:40:56 +0800

    add -r for getjar

--- batch/getjar.bat
+++ batch/getjar.bat
@@ -11,17 +11,20 @@ set repo=http://repo2.maven.org/maven2
 set groupId=
 set artifactId=
 set version=
-
 for %%e in ( %* ) do (
 	if %%e gtr 0 (
-		if not defined groupId (
-			set groupId=%%e
+		set c=%%e
+		if "!c!" equ "-r" (
+			mvn help:evaluate -Dexpression="settings.localRepository"|findstr /R "^[^\[]">%TEMP%\mvn.out
+			for /f "usebackq delims=" %%l in (%TEMP%\mvn.out) do set localRepo=%%l
 		) else (
-			if not defined artifactId (
-				set artifactId=%%e
+			if not defined groupId (
+				set groupId=!c!
 			) else (
-				if not defined version (
-					set version=%%e
+				if not defined artifactId (
+					set artifactId=!c!
+				) else (
+					if not defined version set version=!c!
 				)
 			)
 		)
@@ -32,12 +35,19 @@ if defined groupId if defined artifactId if defined version goto :execute
 goto :invalid_parameters
 
 :execute
+if not defined localRepo (
+	set localRepo=.
+) else (
+	set localRepo=!localRepo!\!groupId:.=\!\!artifactId!\!version!
+)
 set jar=!artifactId!-!version!.jar
 set src=!artifactId!-!version!-sources.jar
 set parent=!repo!/!groupId:.=/!/!artifactId!/!version!
-echo going to get: !groupId!:!artifactId!:!version!
+echo going to get: !groupId!:!artifactId!:!version! to !localRepo!
+pushd !localRepo!>nul
 if not exist !jar! axel -n 4 -o !jar! !parent!/!jar!
 if not exist !src! axel -n 4 -o !src! !parent!/!src!
+popd>nul
 goto :end
 
 :invalid_parameters
