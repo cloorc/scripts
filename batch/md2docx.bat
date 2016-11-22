58e78be7d6940a87f5a184cf8e75a041e46316db
soiff <wittcnezh@foxmail.com>
2016-11-22 09:18:53 +0800

    add md2docx batch script

--- batch/md2docx.bat
+++ batch/md2docx.bat
@@ -2,17 +2,19 @@
 
 setlocal enabledelayedexpansion
 
-set src=README.md
-if not exist !src! goto :nosuchfile
-
-set dst=README.docx
-if "%~1" neq "" set "dst=%~1"
-
-pandoc -f markdown_github -t docx !src! -o !dst!
+:parse
+if "%~x1" equ ".md" call :convert "%~1"
+shift /1
 goto :exit
 
+:convert
+set /p "ign=converting [%~1] to [%~dpn1.docx] ... " < nul
+pandoc -f markdown_github -t docx "%~1" -o "%~dpn1.docx"
+echo done
+goto :eof
+
 :nosuchfile
-echo README.md not found!
+echo !src! or !dst! does not exist!
 goto :eof
 
 :exit
