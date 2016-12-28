21a68573363b60a931ba4edee8d064c69724a2c2
soiff <wittcnezh@foxmail.com>
2016-12-28 14:43:23 +0800

    use internal docker image

--- bash/hsf.sh
+++ bash/hsf.sh
@@ -26,11 +26,26 @@ finalName=`mvn help:evaluate -Dexpression=project.build.finalName 2>/dev/null|gr
 	exit -1
 }
 
+[ ! -e target/deploy ] && mkdir target/deploy
+[ ! -e target/deploy/$finalName.war ] && cp target/$finalName.war target/deploy/$finalName.war
+
+container=edas-${PWD##*/}
+
+count=`docker ps -f name=$container|wc -l`
+
+[ $count -eq 2 ] && docker stop $container
+
+count=`docker ps -af name=$container|wc -l`
+
+[ $count -eq 2 ] && docker rm $container
+
 #/d/source/directory
-local_wd=$PWD/target/$finalName.war
+local_wd=$PWD/target/deploy
 #\\d\\source\\directory
 local_wd=${local_wd//\//\\}
 local_wd=${local_wd#\\}
-local_wd=${local_wd/\\/:}
+local_wd=${local_wd/\\/:\\}
 
-docker run --name=edas-${PWD##*/} -d -p $1:5005 -v $local_wd:/opt/taobao-tomcat-7.0.59/deploy/$finalName.war  --add-host=jmenv.tbsite.net:$2 --add-host=config.tesir.top:192.168.103.101  index.tenxcloud.com/revolc/edas:latest
+info "${local_wd//\\/\\\\}"
+
+docker run --name=$container -d -p $1:5005 -p ${3:-8080}:8080 -v $local_wd:/home/tomcat/deploy -e JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=0.0.0.0:5005,suspend=n,server=y" --add-host=jmenv.tbsite.net:$2 --add-host=config.tesir.top:192.168.103.101  docker.tesir.top/ci/taobao-tomcat:v1.1
