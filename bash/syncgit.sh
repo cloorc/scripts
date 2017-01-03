8cf14150c00b293f246be96f317dec4bfc8b680f
soiff <wittcnezh@foxmail.com>
2017-01-03 11:29:33 +0800

    remove -e for set

--- bash/syncgit.sh
+++ bash/syncgit.sh
@@ -1,27 +1,44 @@
 #!/usr/bin/env bash
-set -e
+
+function error() {
+	echo -e "\033[31merror $*\033[0m"
+}
+
+function exit() {
+	echo -e "\033[31mexiting ... $*\033[0m"
+}
+
+function usage() {
+	echo -e "\033[31musage: ${BASH_SOURCE##*/} [ folder [ repository ] ]\033[0m"
+}
+
+trap error ERR
+trap exit EXIT
 
 function single() {
     if [ -d $1 ] ;then
         pushd $1
         if [ -d .git ] ;then
-        	if [ -z "$2" ] ;then
-            	rep=`git remote -v|grep "origin\s\+.*\s\+(push)"|awk -F/ '{print $2}'|awk '{print $1}'`
+        	if [ -z "${2:-}" ] ;then
+            	rep="lolegends/`git remote -v|grep "origin\s\+.*\s\+(push)"|awk -F/ '{print $2}'|awk '{print $1}'`"
             else
             	rep=$2
             fi
-            git remote add oschina git@git.oschina.net:lolegends/$rep
-            git push oschina master
-            git remote remove oschina
+        	git remote add oschina git@git.oschina.net:$rep
+        	git remote -v
+        	git push oschina master
+        	git remote remove oschina
         fi
-        popd $1
+        popd
     fi
 }
 
-if [ -d .git ] ;then
-    single . $*
-elif [ -n "$*" ] ;then
+usage
+
+if [ -n "$*" ] ;then
 	single $*
+elif [ -d .git ] ;then
+    single . $*
 else
     for d in * ;do
         single $d
